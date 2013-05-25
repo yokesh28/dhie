73dd63765966b1b953fb0f959f8d917a82540449
Lokesh <yokesh.ganesan@gmail.com>
2013-05-25 12:02:42 +0530

    Signed-off-by: Lokesh <yokesh.ganesan@gmail.com>

--- index.php
+++ index.php
@@ -23,7 +23,7 @@
 *  @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
 *  International Registered Trademark & Property of PrestaShop SA
 */
-
+die();
 require(dirname(__FILE__).'/config/config.inc.php');
 Dispatcher::getInstance()->dispatch();
 
